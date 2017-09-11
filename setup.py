dff7fb664b6cc9f153cd02e75e287f5269ea430d
Mathieu Tarral <mathieu.tarral@f-secure.com>
2017-09-11 12:49:24 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#
+from distutils.core import setup
+
+import rfc822py3
+
+
+setup(name='rfc822py3',
+      version='0.1',
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          ],
+      py_modules=['rfc822py3'])
